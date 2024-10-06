8089e65a64943278623f1c3b07e6fe4550c921bf
leonardojgm <cookie.maia@gmail.com>
2024-10-06 19:38:00 -0300

    Junção de projeto com story e game

---
+++ js/story.js
@@ -0,0 +1,13 @@
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function moveSlide(direction) {
+    currentIndex += direction;
+
+    if (currentIndex < 0) currentIndex = totalSlides - 1;
+    
+    if (currentIndex >= totalSlides) currentIndex = 0;
+
+    slides.style.transform = `translateX(-${currentIndex * 1400}px)`;
+}
